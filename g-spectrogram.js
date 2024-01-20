0ddbc737d7dc658f34867d7dc1952ad24e82d807
sk408 <75577242+sk408@users.noreply.github.com>
2024-01-19 19:21:06 -0800

    Update g-spectrogram.js

--- g-spectrogram.js
+++ g-spectrogram.js
@@ -1,19 +1,3 @@
-if (!navigator.mediaDevices?.enumerateDevices) {
-  console.log("enumerateDevices() not supported.");
-} else {
-  // List cameras and microphones.
-  navigator.mediaDevices
-    .enumerateDevices()
-    .then((devices) => {
-      devices.forEach((device) => {
-        console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
-      });
-    })
-    .catch((err) => {
-      console.error(`${err.name}: ${err.message}`);
-    });
-}
-
 Polymer('g-spectrogram', {
   // Show the controls UI.
   controls: false,
